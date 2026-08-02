ea7cfff3e8acf4037ba415db8da0a032999a16de
deesri-tlc <deepak.srivastava@tlconsulting.com.au>
2026-08-02 19:09:45 +1000

    release: fill brew formula sha256 from crates.io source crate

--- install/brew/deectx.rb
+++ install/brew/deectx.rb
@@ -2,7 +2,7 @@ class Deectx < Formula
   desc "Local-first PII-masking proxy for AI coding tools"
   homepage "https://github.com/deectxone/deectx"
   url "https://crates.io/api/v1/crates/deectx/0.1.0/download"
-  sha256 "fill-from-release"
+  sha256 "47f4fd30de1f9773f1b8a07968c27c645879bb1bf4435165f5d912cb13df55c1"
   license "Apache-2.0"
 
   depends_on "rust" => :build
